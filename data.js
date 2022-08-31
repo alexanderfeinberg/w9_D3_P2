efc7ae93bb21ba75a6b8b3701838a3e4b870409b
alexander1fein <feinberg.alexander@gmail.com>
2022-08-31 13:44:06 -0400

    Done

--- data.js
+++ data.js
@@ -1,1 +1,14 @@
 // Your code here
+window.addEventListener("DOMContentLoaded", event=>{
+    const addBtn = document.body.querySelector('#add')
+    const shoppingList = document.body.querySelector('ul')
+    const inputName = document.body.querySelector('#name')
+    const inputType = document.body.querySelector('#type')
+    addBtn.addEventListener("click", event =>{
+        const liItem = document.createElement('li')
+        const text = `${inputName.value}, ${inputType.value}`
+        liItem.innerText = text
+        liItem.setAttribute('data-type',inputType.value)
+        shoppingList.appendChild(liItem)
+    })
+})
